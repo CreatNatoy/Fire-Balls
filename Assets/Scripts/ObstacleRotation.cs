52f231c9d744ae5e1a7ff154c2be83ab3282760d
CreatNatoy <76531899+CreatNatoy@users.noreply.github.com>
2021-11-04 10:36:31 +0100

    Update 2

---
+++ Assets/Scripts/ObstacleRotation.cs
@@ -0,0 +1,14 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleRotation : MonoBehaviour
+{
+    [SerializeField] private float _animationDuration;
+
+    private void Start()
+    {
+        transform.DORotate(new Vector3(0, 360, 0), _animationDuration, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Yoyo); 
+    }
+}
